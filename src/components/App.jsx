e6307cbe36c15329d99f7588aeb6dbbdf24545ab
Vlad Shchelkunov <specter.ode@gmail.com>
2022-07-14 00:57:22 +0300

    make 1 task without style

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,13 @@
+import Profile from './Profile/Profile';
+// import Statistics from './components/Statistics';
+// import FriendList from './components/FriendList';
+// import TransactionHistory from './components/TransactionHistory';
+
+import user from './user.json';
+// import data from './FriendListon';
+// import transactions from './transactions.json';
+// import friends from './friends.json';
+
 export const App = () => {
   return (
     <div
@@ -7,10 +17,19 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        // stats={user.stats}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
     </div>
   );
 };
